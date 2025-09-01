b87cf3afcd16aa2ece4301ebd61845b3162c9e86
serpahime <drazhanartem@gmail.com>
2025-09-01 21:46:35 +0200

    Delete start-backend.bat

--- start-backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Discord Tracker Pro Backend...
-echo.
-echo Make sure config.env is properly configured
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-npm run dev
-pause
